be993471789e95e516bae46d8f106d9f9bf569d0
daiy <daiy@hiscene.com>
2019-02-02 16:30:51 +0800

    Update

--- tool/EveryCodesWithThis.py
+++ tool/EveryCodesWithThis.py
@@ -1,10 +1,43 @@
 # -*- coding: utf-8 -*-
 """
-XXXXXXXXX
+Language:
+Goal:
+PS:
 """
 import sys
 import os
 import time
+
+### Common utilities
+def safeDirectory(fDir):
+    if str == type(fDir):
+        if os.path.sep == fDir[-1]:
+            safeDir = fDir
+        else:
+            safeDir = fDir + os.path.sep
+    else:
+        print("Error type of input!!!")
+        sys.exit(0)
+    return safeDir
+
+def makeDirs(fDir, fExistencePermitted = True):
+    safeDir = fDir
+    try:
+        if not os.path.exists(safeDir):
+            os.makedirs(safeDir)
+        else:
+            if not fExistencePermitted:
+                print("The folder had been existed!!!")
+                sys.exit(0)
+            else:
+                pass
+    except Exception as e:
+        print("Exception occured!!!")
+        print(e)
+        sys.exit(0)
+    else:
+        print("Create: " + safeDir + "    OK")
+        return safeDir
 
 ### Definition region(Class, Functions, Constants)
 
@@ -15,8 +48,7 @@ if "__main__" == __name__:
 ### Parameters region
 
 ### Job region
-    print("Do something here~")
-    pass
+    print("Do something~")
 
     print(os.linesep)
     print(time.strftime(timeStampFormat, time.localtime()))
---
+++ tool/ExtractFiles.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Language:   python3
+Goal:       Extract files into one folder
+"""
+import sys
+import os
+import time
+
+import shutil
+### Definition region(Class, Functions, Constants)
+def doJob(fRootDir, fDstDir):
+    rootDir = fRootDir + os.path.sep
+    if os.path.exists(rootDir):
+        pass
+    else:
+        print("No source!!!")
+        sys.exit(0)
+
+    dstDir = fDstDir + os.path.sep
+    if not os.path.exists(dstDir):
+        os.makedirs(dstDir)
+    else:
+        pass
+
+    for rt, dirs, files in os.walk(rootDir):
+        for i in files:
+            old = os.path.join(rt, i)
+            print(old)
+
+            new = rt.split(os.path.sep)[-1]
+            # print(new)
+            new = dstDir + new + '-' + i
+            print(new)
+            print()
+            shutil.copyfile(old, new)
+    return
+
+if "__main__" == __name__:
+    print(sys.version)
+    timeStampFormat = "%Y-%m-%d %H:%M:%S"
+    print(time.strftime(timeStampFormat, time.localtime()))
+### Parameters region
+    cSrcDir = "/home/devin/Downloads/tmp/hiscene_inspect/"
+    cDstDir = "/home/devin/Downloads/tmp/Extract/ChangeIt/"
+### Job region
+    doJob(cSrcDir, cDstDir)
+
+    print(os.linesep)
+    print(time.strftime(timeStampFormat, time.localtime()))
